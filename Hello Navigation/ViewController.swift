b1514b174f2c6664eb20325b9d0439eab36be91e
Vinukonda,Sai Manikanta Durga Prasad <s533980@ch140037m.nwmissouri.edu>
2019-02-21 13:47:33 -0600

    Initial Commit

---
+++ Hello Navigation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello Navigation
+//
+//  Created by Vinukonda,Sai Manikanta Durga Prasad on 2/21/19.
+//  Copyright © 2019 Vinukonda,Sai Manikanta Durga Prasad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
